cc375908c5ca9e70307904d137bb299843d9ca93
Elementals-technical <65289363+Elementals-technical@users.noreply.github.com>
2024-01-26 18:16:30 +0200

    test:commit

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -16,6 +16,7 @@ export const bhoustonAuth = {
   orgId: '04015bb6-401d-47f8-97c0-dd6fa759c441',
   publicToken: 'b107f4dd-51d7-48f7-8fa3-c76dcf663f8a'
 };
+// console.log();
 
 const assetId = '32ba8c20-d54a-46d2-a0bb-0339c71e7dc6'; // product
 
