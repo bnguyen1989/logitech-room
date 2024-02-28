8a4ab25e0d55cd1725d78ac8cff12308c78dd7c6
Andrii Korniienko <akornienko882@gmail.com>
2024-02-28 11:32:35 +0200

    [ui_branch] change settings assetId

--- src/utils/threekitUtils.ts
+++ src/utils/threekitUtils.ts
@@ -4,15 +4,15 @@ export const ConfigData = {
   host: "preview.threekit.com",
   orgId: "04015bb6-401d-47f8-97c0-dd6fa759c441",
   publicToken: "b107f4dd-51d7-48f7-8fa3-c76dcf663f8a",
-  assetId: "32ba8c20-d54a-46d2-a0bb-0339c71e7dc6",
+  assetId: "f859d9d2-dbea-408f-87ed-9c17c959f452",
 };
 
 export const getRoomAssetId = (roomSize: string) => {
   switch (roomSize) {
     case RoomSizeName.Medium:
-      return "78413aac-16a7-473a-b0bd-2741655ed43f";
+      return "f859d9d2-dbea-408f-87ed-9c17c959f452";
     default:
-      return "78413aac-16a7-473a-b0bd-2741655ed43f";
+      return "f859d9d2-dbea-408f-87ed-9c17c959f452";
   }
 };
 
