d234d4bea9bee85641c5c09e36a7d8b7353a6a0d
Andrii Korniienko <akornienko882@gmail.com>
2024-02-29 13:23:09 +0200

    [ui_branch] added Marketo form and style and correct logic

--- src/components/Modals/SetupModal/SetupModal.tsx
+++ src/components/Modals/SetupModal/SetupModal.tsx
@@ -3,11 +3,7 @@ import { CloseSVG } from "../../../assets";
 import { useAppSelector } from "../../../hooks/redux";
 import { setMySetupModal } from "../../../store/slices/modals/Modals.slice";
 import { getSetupModalData } from "../../../store/slices/modals/selectors/selectors";
-import { Button } from "../../Buttons/Button/Button";
 import { IconButton } from "../../Buttons/IconButton/IconButton";
-import { CheckBox } from "../../CheckBox/CheckBox";
-import { Field } from "../../Fields/Field/Field";
-import { Select } from "../../Fields/Select/Select";
 import { ModalContainer } from "../ModalContainer/ModalContainer";
 import s from "./SetupModal.module.scss";
 import { useNavigate } from "react-router-dom";
@@ -16,9 +12,11 @@ import { ConfigData } from "../../../utils/threekitUtils";
 import { Application } from "../../../models/Application";
 import { getSelectedConfiguratorCards } from "../../../store/slices/ui/selectors/selectors";
 import { ItemCardI } from "../../../store/slices/ui/type";
-import { useState } from "react";
+import { useEffect } from "react";
+import "./form.css";
 
 declare const app: Application;
+declare const MktoForms2: any;
 
 export const SetupModal: React.FC = () => {
   const navigate = useNavigate();
@@ -27,11 +25,29 @@ export const SetupModal: React.FC = () => {
   const selectedCards: Array<ItemCardI> = useAppSelector(
     getSelectedConfiguratorCards
   );
-  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const handleClose = () => {
     dispatch(setMySetupModal({ isOpen: false }));
   };
+
+  useEffect(() => {
+    if (!isOpen) return;
+    MktoForms2.loadForm("//info.logitech.com", "201-WGH-889", 18414);
+
+    MktoForms2.whenReady((form: any) => {
+      form.onSubmit(() => {
+        createOrder().then(() => {
+          dispatch(setMySetupModal({ isOpen: false }));
+          navigate("/room", { replace: true });
+        });
+        return false;
+      });
+      const button = document.querySelector(".mktoButton");
+      if (button) {
+        button.textContent = "See my results";
+      }
+    });
+  }, [isOpen]);
 
   const createOrder = async () => {
     const cardData = selectedCards.map((card) => {
@@ -64,18 +80,6 @@ export const SetupModal: React.FC = () => {
     });
   };
 
-  const handleSeeResults = () => {
-    setIsSubmitting(true);
-    createOrder()
-      .then(() => {
-        dispatch(setMySetupModal({ isOpen: false }));
-        navigate("/room", { replace: true });
-      })
-      .finally(() => {
-        setIsSubmitting(false);
-      });
-  };
-
   if (!isOpen) return null;
 
   return (
@@ -98,88 +102,7 @@ export const SetupModal: React.FC = () => {
         </div>
 
         <div className={s.form}>
-          <Field placeholder={"First Name"} required />
-          <Field placeholder={"Last Name"} required />
-          <Field placeholder={"Email Address"} required />
-          <Field placeholder={"Phone Number"} required />
-          <Field placeholder={"Company Name"} required />
-          <Select
-            onSelect={() => {}}
-            options={[
-              { label: "Option 1", value: "1" },
-              { label: "Option 2", value: "2" },
-              { label: "Option 3", value: "3" },
-            ]}
-            placeholder={"Job Function (L)"}
-            required
-          />
-          <Select
-            onSelect={() => {}}
-            options={[
-              { label: "Option 1", value: "1" },
-              { label: "Option 2", value: "2" },
-              { label: "Option 3", value: "3" },
-            ]}
-            placeholder={"Industry"}
-            required
-          />
-          <Select
-            onSelect={() => {}}
-            options={[
-              { label: "Option 1", value: "1" },
-              { label: "Option 2", value: "2" },
-              { label: "Option 3", value: "3" },
-            ]}
-            placeholder={"Country"}
-          />
-          <Select
-            onSelect={() => {}}
-            options={[
-              { label: "Option 1", value: "1" },
-              { label: "Option 2", value: "2" },
-              { label: "Option 3", value: "3" },
-            ]}
-            placeholder={"Time Frame to Purchase:"}
-          />
-          <Select
-            onSelect={() => {}}
-            options={[
-              { label: "Option 1", value: "1" },
-              { label: "Option 2", value: "2" },
-              { label: "Option 3", value: "3" },
-            ]}
-            placeholder={"Are you a Logitech Partner or Reseller?"}
-          />
-        </div>
-        <div className={s.additional_info}>
-          <div className={s.support_link}>
-            <div>Need Product Support? Please</div>
-            <a href="https://support.logi.com/hc/" target="_blank">
-              Click Here.
-            </a>
-          </div>
-          <CheckBox
-            value={false}
-            onChange={() => {}}
-            text={
-              "Would you like to be contacted by Logitech about this project?"
-            }
-          />
-          <CheckBox
-            value={false}
-            onChange={() => {}}
-            text={
-              "By supplying my contact information, I authorize Logitech to contact me with personalized communications about Logitech's products and services, which per <a href='https://www.logitech.com/en-us/legal/web-privacy-policy.html' style='color: var(--main-color-text);'>Logitech's Privacy Policy</a> I may opt-out of at any time."
-            }
-          />
-        </div>
-        <div className={s.actions}>
-          <Button
-            text={"See my results"}
-            onClick={handleSeeResults}
-            variant={"contained"}
-            disabled={isSubmitting}
-          />
+          <form id="mktoForm_18414"></form>
         </div>
       </div>
     </ModalContainer>
