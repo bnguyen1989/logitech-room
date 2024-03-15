8afe345577679a790dd3d74a239f2f6f4289a1c4
Andrii Korniienko <akornienko882@gmail.com>
2024-03-15 09:52:14 +0200

    [architecture] correct get title selected cards for section loader

--- src/pages/configurator/Content/LoaderSection/Card/Card.tsx
+++ src/pages/configurator/Content/LoaderSection/Card/Card.tsx
@@ -1,16 +1,31 @@
 import React from "react";
 import s from "./Card.module.scss";
+import { useAppSelector } from "../../../../../hooks/redux";
+import {
+  getCardByKeyPermission,
+  getStepNameByKeyPermission,
+  getTitleCardByKeyPermission,
+} from "../../../../../store/slices/ui/selectors/selectors";
 
 interface PropsI {
-  image: string;
-  title: string;
+  keyItemPermission: string;
 }
 export const Card: React.FC<PropsI> = (props) => {
-  const { image, title } = props;
+  const { keyItemPermission } = props;
+  const stepName = useAppSelector(
+    getStepNameByKeyPermission(keyItemPermission)
+  );
+  const card = useAppSelector(
+    getCardByKeyPermission(stepName, keyItemPermission)
+  );
+  const title = useAppSelector(
+    getTitleCardByKeyPermission(stepName, keyItemPermission)
+  );
+
   return (
     <div className={s.container}>
       <div className={s.image}>
-        <img src={image} alt={title} />
+        <img src={card.image} alt={title} />
       </div>
       <div className={s.title}>{title}</div>
     </div>
