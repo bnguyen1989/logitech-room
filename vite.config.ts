f67b4e6392d81e2ca6256104d0e6b6f1a3852fd3
technical <technical@elementalsweb.com>
2024-03-21 16:09:12 +0200

    update: build main js file

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  define: {
-    global: 'globalThis',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/index.js`, // Фиксирует имя выходного файла для точек входа
+        chunkFileNames: `assets/[name].js`, // Управляет именованием неразрезанных частей
+        assetFileNames: `assets/[name].[ext]`, // Управляет именованием остальных ресурсов (CSS, картинки и т.д.)
+      },
+    },
   },
-})
+  define: {
+    global: "globalThis",
+  },
+});
