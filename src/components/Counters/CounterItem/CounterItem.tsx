0836bd1322eaf2e994dffcbc8b310058a32dcdc6
Andrii Korniienko <akornienko882@gmail.com>
2024-03-26 14:39:20 +0200

    [architecture] added the ability to highlight a card when we increase the quantity by 1

--- src/components/Counters/CounterItem/CounterItem.tsx
+++ src/components/Counters/CounterItem/CounterItem.tsx
@@ -2,6 +2,7 @@ import { useAppSelector } from "../../../hooks/redux";
 import { Application } from "../../../models/Application";
 import {
   getActiveStep,
+  getAssetFromCard,
   getCardByKeyPermission,
   getPropertyCounterCardByKeyPermission,
 } from "../../../store/slices/ui/selectors/selectors";
@@ -20,18 +21,21 @@ export const CounterItem: React.FC<PropsI> = (props) => {
   const card = useAppSelector(
     getCardByKeyPermission(activeStep, keyItemPermission)
   );
+  const cardAsset = useAppSelector(getAssetFromCard(card));
   const count = useAppSelector(
     getPropertyCounterCardByKeyPermission(activeStep, keyItemPermission)
   );
-
-  console.log("count", count);
-  
 
   if (!card || !card.counter || count === undefined) return null;
 
   const { min, max, threekit } = card.counter;
 
   const handleChange = (value: number) => {
+    if (value > count && value === 1) {
+      const attributeName = card.dataThreekit.attributeName;
+      app.addItemConfiguration(attributeName, cardAsset.id, card.keyPermission);
+      return
+    }
     app.changeCountItemConfiguration(
       threekit.key,
       value.toString(),
