0b18be3eb3f7ce053fc9153644d0efcd745ea8c8
technical <technical@elementalsweb.com>
2024-03-26 15:22:20 +0200

    update:Docker and prisma config

--- src/services/api/Server/ServerApi.ts
+++ src/services/api/Server/ServerApi.ts
@@ -3,7 +3,7 @@ import { BaseApi } from "../BaseApi";
 // http://localhost:80/products/en-us
 export class ServerApi extends BaseApi {
   constructor() {
-    const baseUrl = `http://localhost:80/`;
+    const baseUrl = `/`;
     super(baseUrl);
   }
 
