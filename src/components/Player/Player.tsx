d0cf25eefddfa3cbb20f363d19f4e729bfbb1938
Andrii Korniienko <akornienko882@gmail.com>
2024-04-12 16:27:16 +0300

    [rooms_logic] change version cacheScope

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -46,7 +46,7 @@ export const Player: React.FC = () => {
         auth={bhoustonAuth}
         resolver={ExporterResolver({
           cache: true,
-          cacheScope: "v12",
+          cacheScope: "v13",
           mode: "experimental",
           settings: {
             prune: {
