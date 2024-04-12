38cd5011741d02f3b6b84e686e8322f14ac570ad
Andrii Korniienko <akornienko882@gmail.com>
2024-04-12 17:03:30 +0300

    [rooms_logic] improve code detected need download dataTable level1

--- src/models/command/ChangeStepCommand.ts
+++ src/models/command/ChangeStepCommand.ts
@@ -1,20 +1,14 @@
 import { Command } from "./Command";
 import { Configurator } from "../configurator/Configurator";
 import { ChangeStepBehavior } from "./behavior/ChangeStepBehavior";
-import { DirectionStep, StepName } from "../../utils/baseUtils";
+import { StepName } from "../../utils/baseUtils";
 export class ChangeStepCommand extends Command {
   public name: string = "ChangeStepCommand";
   public stepName: StepName;
-  public direction: DirectionStep;
 
-  constructor(
-    configurator: Configurator,
-    stepName: StepName,
-    direction: DirectionStep
-  ) {
+  constructor(configurator: Configurator, stepName: StepName) {
     super(configurator);
     this.stepName = stepName;
-    this.direction = direction;
     this.behaviors.push(new ChangeStepBehavior());
   }
 
