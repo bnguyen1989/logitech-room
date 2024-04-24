1eac6c1a9fdc5a1d8c93cd4ce50ab04593a7bedb
Andrii Korniienko <akornienko882@gmail.com>
2024-04-24 12:11:43 +0300

    [rooms_logic] improve logic annotation modal for view image in blade 2

--- src/components/Modals/AnnotationModal/AnnotationModal.tsx
+++ src/components/Modals/AnnotationModal/AnnotationModal.tsx
@@ -1,6 +1,9 @@
 import { CloseSVG } from "../../../assets";
 import { useAppSelector } from "../../../hooks/redux";
-import { getAnnotationModalData, getDataForAnnotationModal } from "../../../store/slices/modals/selectors/selectors";
+import {
+  getAnnotationModalData,
+  getDataForAnnotationModal,
+} from "../../../store/slices/modals/selectors/selectors";
 import { Button } from "../../Buttons/Button/Button";
 import { IconButton } from "../../Buttons/IconButton/IconButton";
 
@@ -11,15 +14,16 @@ import { useDispatch } from "react-redux";
 import { setAnnotationItemModal } from "../../../store/slices/modals/Modals.slice";
 import {
   getLangForModalProduct,
-  getLangProductImage, 
+  getLangProductImage,
 } from "../../../store/slices/ui/selectors/selectoreLangProduct";
-import { ColorSwitcherItem } from "../../ColorSwitchers/ColorSwitcherItem/ColorSwitcherItem"; 
+import { ColorSwitcherItem } from "../../ColorSwitchers/ColorSwitcherItem/ColorSwitcherItem";
 import { Annotation } from "./AnnotationDataProcces";
 
 export const AnnotationModal: React.FC = () => {
   const dispatch = useDispatch();
-  const { isOpen, product, card, keyPermission } = useAppSelector(getAnnotationModalData) as any;
-
+  const { isOpen, product, card, keyPermission } = useAppSelector(
+    getAnnotationModalData
+  ) as any;
 
   const dataProduct: any = useAppSelector(getLangForModalProduct(product));
 
@@ -31,7 +35,6 @@ export const AnnotationModal: React.FC = () => {
     getDataForAnnotationModal(keyPermission, card)
   );
 
-
   if (!card) return <></>;
 
   const handleClick = () => {
@@ -41,37 +44,33 @@ export const AnnotationModal: React.FC = () => {
       return;
     }
 
-    if(!threekitAsset) return;
+    if (!threekitAsset) return;
 
-    app.addItemConfiguration(
-      attributeName,
-      threekitAsset.id,
-      keyPermission
-    );
+    app.addItemConfiguration(attributeName, threekitAsset.id, keyPermission);
   };
-
 
   if (!dataProduct) return <></>;
 
   const annotation = new Annotation(dataProduct);
 
-  const featureList2A = annotation.getFeatures('fetures2A')
+  const featureList2A = annotation.getFeatures("fetures2A");
 
-  const featureList3A = annotation.getFeatures('fetures3A')
+  const featureList3A = annotation.getFeatures("fetures3A");
 
   const handleClose = () => {
     dispatch(setAnnotationItemModal({ isOpen: false }));
   };
-
-
 
   if (!isOpen) return null;
 
   const style2A = annotation.hasGap2(featureList2A) ? s.gap_2_box : s.cards;
   const style3A = annotation.hasGap2(featureList3A) ? s.gap_2_box : s.cards;
 
-  const style2AGrey = !dataProduct['Headline'] ? s.sectionsGrey : '';
-  const style3AGrey = !dataProduct['Headline'] && !annotation.hasShow(featureList2A) ? s.sectionsGrey : '';
+  const style2AGrey = !dataProduct["Headline"] ? s.sectionsGrey : "";
+  const style3AGrey =
+    !dataProduct["Headline"] && !annotation.hasShow(featureList2A)
+      ? s.sectionsGrey
+      : "";
 
   return (
     <ModalContainer>
@@ -125,32 +124,43 @@ export const AnnotationModal: React.FC = () => {
             </div>
           )}
 
-          {dataProduct["Headline"] && <div className={`${s.section_video} ${s.sectionsGrey}`}>
-            <div className={s.left_content}>
-              {dataProduct && dataProduct["Headline"] && (
-                <div className={s.title_video}>{dataProduct["Headline"]}</div>
-              )}
+          {dataProduct["Headline"] && (
+            <div className={`${s.section_video} ${s.sectionsGrey}`}>
+              <div className={s.left_content}>
+                {dataProduct && dataProduct["Headline"] && (
+                  <div className={s.title_video}>{dataProduct["Headline"]}</div>
+                )}
 
-              {dataProduct && dataProduct["Description"] && (
-                <div className={s.decs_video}>{dataProduct["Description"]}</div>
-              )}
-            </div>
-            <div className={s.right_content}>
-              <div className={s.video}>
-                {dataProduct &&
-                  dataProduct["Image|Video"] &&
-                  dataProduct["Image|Video"]["Video"] && (
-                    <VideoPlayer path={dataProduct["Image|Video"]["Video"]} />
-                  )}
+                {dataProduct && dataProduct["Description"] && (
+                  <div className={s.decs_video}>
+                    {dataProduct["Description"]}
+                  </div>
+                )}
+              </div>
+              <div className={s.right_content}>
+                <div className={s.video}>
+                  {dataProduct &&
+                    dataProduct["Image|Video"] &&
+                    (dataProduct["Image|Video"]["Video"] ? (
+                      <VideoPlayer path={dataProduct["Image|Video"]["Video"]} />
+                    ) : Object.keys(dataProduct["Image|Video"])[0] ? (
+                      <img
+                        src={
+                          dataProduct["Image|Video"][
+                            Object.keys(dataProduct["Image|Video"])[0]
+                          ]
+                        }
+                        alt="image"
+                      />
+                    ) : null)}
+                </div>
               </div>
             </div>
-          </div>}
+          )}
 
           {annotation.hasShow(featureList2A) && (
             <div className={`${style2A} ${style2AGrey}`}>
               {featureList2A.map((feature: any) => {
-
-
                 return (
                   <div key={feature["HeaderFeature"]} className={s.card}>
                     <div className={s.image}>
@@ -168,12 +178,14 @@ export const AnnotationModal: React.FC = () => {
           {annotation.hasShow(featureList3A) && (
             <div className={`${style3A} ${style3AGrey}`}>
               {featureList3A.map((feature: any) => {
-
-
                 return (
                   <div key={feature["HeaderFeature"]} className={s.card}>
-                    <div className={s.image}>{feature["images"].length > 0 ? <img src={feature["images"][0]} alt="image" /> : <VideoPlayer path={feature["videos"][0]} />}
-
+                    <div className={s.image}>
+                      {feature["images"].length > 0 ? (
+                        <img src={feature["images"][0]} alt="image" />
+                      ) : (
+                        <VideoPlayer path={feature["videos"][0]} />
+                      )}
                     </div>
                     <div className={s.text}>
                       <div className={s.title}>{feature["HeaderFeature"]}</div>
