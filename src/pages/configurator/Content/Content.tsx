d96ec87a4324aa4d02504e745efd01aea3f6e896
Andrii Korniienko <akornienko882@gmail.com>
2024-05-02 18:23:17 +0300

    [rooms_logic] improve ui page configuration and sections for tablet/mobile

--- src/pages/configurator/Content/Content.tsx
+++ src/pages/configurator/Content/Content.tsx
@@ -4,18 +4,19 @@ import { PrepareSection } from "./PrepareSections/PrepareSection";
 import { ConfiguratorSection } from "./ConfiguratorSections/ConfiguratorSection";
 import { LoaderSection } from "./LoaderSection/LoaderSection";
 import { ActionsContent } from "./ActionsContent/ActionsContent";
+import { Heading } from "./Heading/Heading";
 
 interface PropsI {}
 export const Content: React.FC<PropsI> = () => {
   return (
     <div className={s.container}>
+      <Heading />
       <div className={s.content}>
         <PrepareSection />
         <ConfiguratorSection />
       </div>
 
       <ActionsContent />
-
       <LoaderSection />
     </div>
   );
