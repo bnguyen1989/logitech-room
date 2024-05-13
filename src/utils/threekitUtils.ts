e99e64edc4d548eaa4d73e196845de0a9828a2ce
Elementals-technical <65289363+Elementals-technical@users.noreply.github.com>
2024-05-13 19:55:24 +0300

    update: update token for logitech web site

--- src/utils/threekitUtils.ts
+++ src/utils/threekitUtils.ts
@@ -3,7 +3,7 @@ import { RoomSizeName } from "./permissionUtils";
 export const ConfigData = {
   host: "preview.threekit.com",
   orgId: "04015bb6-401d-47f8-97c0-dd6fa759c441",
-  publicToken: "302e1adf-1d52-46f9-9d71-3112789cfd55",
+  publicToken: "10d33d14-2cbb-4e98-8161-99225a65e298",
 };
 
 export const getRoomAssetId = (roomSize: string) => {
