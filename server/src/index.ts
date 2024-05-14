caee07b4064afa1e4faf233adf6ce305b27dc386
Andrii Korniienko <akornienko882@gmail.com>
2024-05-14 18:04:50 +0300

    [rooms_logic] improve logic server generate file for Language

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,7 @@ import cors from "cors";
 import userRouterProduct from "./routes/product";
 import userLang from "./routes/languages";
 import userRouterRoom from "./routes/room";
-import { applyServerHardening } from "./middleware";
+import { dataLangFile } from "./utils/langUtils";
 
 const app = express();
 
@@ -13,7 +13,6 @@ const PORT = process.env.PORT || 80;
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true })); // Для парсинга application/x-www-form-urlencoded
-applyServerHardening(app);
 
 app.use("/products", userRouterProduct);
 app.use("/languages", userLang);
@@ -25,4 +24,7 @@ app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../../dist/index.html"));
 });
 
-app.listen(PORT, () => console.log("listening on port: ", PORT));
+app.listen(PORT, () => {
+  dataLangFile();
+  console.log("listening on port: ", PORT);
+});
--- server/src/middleware/index.ts
+++
@@ -1,6 +0,0 @@
-import { dataLangFile } from "./dataLangMiddleware";
-import { Express } from 'express';
-
-export function applyServerHardening(app: Express): void {
-  app.use(dataLangFile);
-}
