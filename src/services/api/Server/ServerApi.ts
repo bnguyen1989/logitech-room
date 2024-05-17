70efa3f7a584751fe29a820595c3d7317c63b5fb
Andrii Korniienko <akornienko882@gmail.com>
2024-05-17 17:18:31 +0300

    [rooms_logic] update base server url

--- src/services/api/Server/ServerApi.ts
+++ src/services/api/Server/ServerApi.ts
@@ -2,7 +2,7 @@ import { BaseApi } from "../BaseApi";
 
 export class ServerApi extends BaseApi {
   public static getUrlApi() {
-    let link = "https://staging.project--logitech.pages.dev";
+    let link = "https://logitech-staging.3kit.com";
 
     if (process.env.NODE_ENV === "development") {
       link = "http://localhost:3000";
