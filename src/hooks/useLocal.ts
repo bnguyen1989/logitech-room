bb0afaed5fe05c39e38f99afb65f793d3383b000
Andrii Korniienko <akornienko882@gmail.com>
2024-05-18 17:32:16 +0300

    [rooms_logic] added language-specific labeling processing

--- src/hooks/useLocal.ts
+++ src/hooks/useLocal.ts
@@ -11,7 +11,25 @@ export const useLocale = () => {
   const localParam = searchParams.get("locale");
   const locale = useAppSelector(getLocale);
 
+  const getLocaleFromPathName = () => {
+    const pathName = window.location.pathname;
+    const pathNameParts = pathName.split("/");
+    const locale = pathNameParts.find((part) =>
+      ["es_mx", "es_es", "de_de", "es_us"].includes(part)
+    );
+    if (locale === "es_us") return "es-us";
+    if (locale === "es_es") return "es-ES";
+    if (locale === "de_de") return "de";
+    if (locale === "es_mx") return "es-MX";
+    return locale;
+  };
+
   useEffect(() => {
+    const localPathName = getLocaleFromPathName();
+    if (localPathName) {
+      dispatch(updateLocale(localPathName));
+      return;
+    }
     if (!localParam) {
       dispatch(updateLocale("en-us"));
       return;
