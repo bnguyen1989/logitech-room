a2b392faa4f4c2947761dabcbcccb41d5fbdd5dc
technical <technical@elementalsweb.com>
2024-06-13 13:17:30 +0300

    update: refact MktoForms2

--- src/components/Form/MarketoForm/MarketoForm.tsx
+++ src/components/Form/MarketoForm/MarketoForm.tsx
@@ -4,46 +4,6 @@ import { useParams } from "react-router-dom";
 const MarketoForm = () => {
   const { idForm } = useParams();
   useEffect(() => {
-    // Function to load a script dynamically
-    const loadScript = (src: any, id: any, onLoad: any) => {
-      if (document.getElementById(id)) return; // Avoid re-adding script
-
-      const script = document.createElement("script");
-      script.src = src;
-      script.id = id;
-      script.async = true;
-      script.onload = onLoad;
-      document.head.appendChild(script);
-    };
-
-    // Initialize Marketo Forms
-    loadScript(
-      "//info.logitech.com/js/forms2/js/forms2.min.js",
-      "mktoFormsScript",
-      () => {
-        debugger;
-
-        window.MktoForms2.loadForm(
-          "//info.logitech.com",
-          "201-WGH-889",
-          idForm,
-          () => {
-            MktoForms2.whenReady((form: any) => {
-              debugger;
-              form.onSuccess((values: any, followUpUrl: any) => {
-                debugger;
-                console.log("values = ", values);
-                console.log("followUpUrl = ", followUpUrl);
-
-                form.getFormElem().hide();
-                return false;
-              });
-            });
-          }
-        );
-      }
-    );
-
     // Initialize Munchkin
     const initMunchkin = () => {
       debugger;
@@ -53,12 +13,21 @@ const MarketoForm = () => {
         window.Munchkin.init("201-WGH-889");
       }
     };
-    debugger;
-    loadScript(
-      "//munchkin.marketo.net/munchkin.js",
-      "munchkinScript",
-      initMunchkin
-    );
+    initMunchkin();
+
+    MktoForms2.loadForm("//info.logitech.com", "201-WGH-889", idForm);
+    MktoForms2.whenReady((form: any) => {
+      debugger;
+      form.onSuccess((values: any, followUpUrl: any) => {
+        debugger;
+        console.log("values = ", values);
+        console.log("followUpUrl = ", followUpUrl);
+
+        form.getFormElem().hide();
+        return false;
+      });
+    });
+ 
   }, []);
 
   return <form id={`mktoForm_${idForm}`}></form>;
