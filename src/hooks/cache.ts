6181d983305e852c7dbabb90389bbcc5373cb932
technical <technical@elementalsweb.com>
2024-06-18 13:15:57 +0300

    update: fix bag rally camera in  phone both

--- src/hooks/cache.ts
+++ src/hooks/cache.ts
@@ -9,7 +9,7 @@ export const useCache = () => {
 
 
   const [cache, setCache] = useState(true);
-  const [keyCache, setKeyCache] = useState<string>("v046");
+  const [keyCache, setKeyCache] = useState<string>("v047");
 
   useEffect(() => {
     if (cacheParam) {
