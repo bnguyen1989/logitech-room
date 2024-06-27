602e494aac48ba766a6c929e4aa2f7f3db0c2528
technical <technical@elementalsweb.com>
2024-06-27 19:20:37 +0300

    update: update chache

--- src/hooks/cache.ts
+++ src/hooks/cache.ts
@@ -9,7 +9,7 @@ export const useCache = () => {
 
 
   const [cache, setCache] = useState(true);
-  const [keyCache, setKeyCache] = useState<string>("v058");
+  const [keyCache, setKeyCache] = useState<string>("v059");
 
   useEffect(() => {
     if (cacheParam) {
