4d4e38e78c7a614c97ee764c7a11078f72221092
Andrii Korniienko <akornienko882@gmail.com>
2024-06-28 18:15:21 +0300

    [ui_branch] update version cache

--- src/hooks/cache.ts
+++ src/hooks/cache.ts
@@ -9,7 +9,7 @@ export const useCache = () => {
 
 
   const [cache, setCache] = useState(true);
-  const [keyCache, setKeyCache] = useState<string>("v061");
+  const [keyCache, setKeyCache] = useState<string>("v062");
 
   useEffect(() => {
     if (cacheParam) {
