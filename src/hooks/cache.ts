8467e5edab5ff3af3376ab249598ae7bc34a7bb0
Andrii Korniienko <akornienko882@gmail.com>
2024-08-09 11:12:41 +0300

    [ui_branch] updated logic permission for mounts huddle camera

--- src/hooks/cache.ts
+++ src/hooks/cache.ts
@@ -9,7 +9,7 @@ export const useCache = () => {
 
 
   const [cache, setCache] = useState(true);
-  const [keyCache, setKeyCache] = useState<string>("v075");
+  const [keyCache, setKeyCache] = useState<string>("v077");
 
   useEffect(() => {
     if (cacheParam) {
