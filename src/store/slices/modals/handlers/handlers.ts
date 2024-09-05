67d6936c35cead3eeec441aeb594acba5e07c5d1
Andrii Korniienko <akornienko882@gmail.com>
2024-09-05 15:38:38 +0300

    [permission] added logic for reset cards when change sight

--- src/store/slices/modals/handlers/handlers.ts
+++ src/store/slices/modals/handlers/handlers.ts
@@ -12,7 +12,7 @@ export const getModalsHandlers = (store: Store) => {
     const state = store.getState();
     if (data instanceof SelectProductModal) {
       const attributeName = data.attributeName;
-      const isShowProductModal = getIsShowProductModal(attributeName)(state);
+      const isShowProductModal = getIsShowProductModal(state);
       if (isShowProductModal) {
         store.dispatch(
           setSelectProductModal({
