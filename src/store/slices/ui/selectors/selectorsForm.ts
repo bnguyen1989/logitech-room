e3af84be77446645feb2bcdf32e1cb5cd186b176
Andrii Korniienko <akornienko882@gmail.com>
2024-12-10 11:10:48 +0200

    [questionnaire] reconnecting the new question list and answer template

--- src/store/slices/ui/selectors/selectorsForm.ts
+++ src/store/slices/ui/selectors/selectorsForm.ts
@@ -1,7 +1,5 @@
 import { RootState } from "../../..";
 import { FormName } from "../../../../utils/baseUtils";
-import { RoleUserName } from "../../../../utils/userRoleUtils";
-import { getRoleData } from "../../user/selectors/selectors";
 import { QuestionFormI } from "../type";
 import { getDataQuestionFormCustomer } from "./selectoteLangPage";
 
@@ -10,15 +8,8 @@ export const getFormDataByName = (name: FormName) => (state: RootState) => {
 };
 
 export const getDataSoftwareQuestionsForm = (state: RootState) => {
-  const userRoleData = getRoleData(state);
-  let dataQuestionForm: QuestionFormI[] = [];
-  if (userRoleData.name === RoleUserName.CUSTOMER) {
-    dataQuestionForm = getDataQuestionFormCustomer("v1")(state);
-  }
-
-  if (userRoleData.name === RoleUserName.PARTNER) {
-    dataQuestionForm = getDataQuestionFormCustomer("v2")(state);
-  }
+  const dataQuestionForm: QuestionFormI[] =
+    getDataQuestionFormCustomer("v3")(state);
 
   const dataState = getFormDataByName(FormName.QuestionFormSoftware)(state);
 
